b86a5cee78926add39d2cf4b05d5e17a32980f39
sachieko <kirazetsubou@gmail.com>
2022-08-03 22:37:28 -0600

    Implemented wordsearch with @WoncheolKim

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,10 +1,10 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word is not present vertical or horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the horizontal word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,92 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the horizontal word is present and not the entire row", function() {
+    const result = wordSearch([
+      ['A', 'W', 'O', 'R', 'D', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WORD');
+
+    assert.isTrue(result);
+  });
+  // Vertical test
+  it("should return true if the vertical word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'O', 'R', 'D', 'W', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'O', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'N', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'C', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'H', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'O', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'L', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WONCHEOL');
+
+    assert.isTrue(result);
+  });
+  it("should return false if the word search is empty", function() {
+    const result = wordSearch([], 'CHELSEA');
+
+    assert.isFalse(result);
+  });
+  it("should return false if the first row is empty", function() {
+    const result = wordSearch([
+      [],
+      ['S', 'E', 'I', 'N', 'F', 'O', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'N', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'C', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'H', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'O', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'L', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CHELSEA');
+
+    assert.isFalse(result);
+  });
+  it("should return true if the word is upside down", function() {
+    const result = wordSearch([
+      ['A', 'C', 'T', 'J', 'B', 'W', 'E', 'P'],
+      ['SPp', 'O', 'I', 'N', 'F', 'O', 'L', 'D'],
+      ['Y', 'K', 'C', 'F', 'Q', 'N', 'A', 'L'],
+      ['H', 'E', 'J', 'T', 'E', 'C', 'R', 'G'],
+      ['W', 'I', 'C', 'S', 'Y', 'H', 'R', 'L'],
+      ['B', 'H', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'C', 'T', 'W', 'A', 'O', 'A', 'I'],
+      ['O', 'A', 'C', 'A', 'K', 'L', 'A', 'S'],
+      ['E', 'S', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SACHIEKO');
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is backwards", function() {
+    const result = wordSearch([
+      ['A', 'C', 'T', 'J', 'B', 'W', 'E', 'P'],
+      ['S', 'O', 'I', 'N', 'F', 'O', 'L', 'D'],
+      ['Y', 'K', 'C', 'F', 'Q', 'N', 'A', 'L'],
+      ['H', 'E', 'J', 'T', 'E', 'C', 'R', 'G'],
+      ['W', 'I', 'C', 'S', 'Y', 'H', 'S', 'L'],
+      ['B', 'H', 'R', 'E', 'N', 'E', 'C', 'B'],
+      ['U', 'C', 'T', 'W', 'A', 'O', 'A', 'I'],
+      ['O', 'A', 'H', 'C', 'A', 'E', 'P', 'S'],
+      ['E', 'S', 'K', 'F', 'Q', 'U', 'P', 'L'],
+    ], 'PEACH');
 
     assert.isTrue(result);
   });
 });
+
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const lotide = require('@sachieko/lotide');
+const wordSearch = (letters, word) => {
+  if (letters.length === 0 || letters[0].length === 0) {
+    return false;
+  }
+  // All the elements in the array, and joining them. Joins horizontal rows only
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const reverseHorizontal = horizontalJoin.map(ls => lotide.reverse(ls));
+  // Checking for the horizontal word below -- V
+  for (const l of horizontalJoin) if (l.includes(word)) return true;
+  for (const l of reverseHorizontal) if (l.includes(word)) return true;
+  // Checking for horizontal ^
+  const transpose = function(matrix) {
+    let result = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      let row = [];
+      for (const element of matrix) {
+        row.push(element[i]);
+      }
+      result.push(row);
     }
-}
+    return result;
+  };
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  const reverseVertical = verticalJoin.map(ls => lotide.reverse(ls));
+  // Checking for vertical --v
+  for (const l of verticalJoin) if (l.includes(word)) return true;
+  for (const l of reverseVertical) if (l.includes(word)) return true;
+  // Checking for vertical word --^
+  return false;
 
-module.exports = wordSearch
+};
+
+module.exports = wordSearch;
+
